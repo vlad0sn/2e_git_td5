8ff5622cd733527b26759adb6fe27e65349d818a
vlad0sn <vladsenechal@gmail.com>
2025-02-02 18:25:06 +0100

    test infra2

--- td5/scripts/tofu/live/lambda-sample/main.tf
+++ td5/scripts/tofu/live/lambda-sample/main.tf
@@ -2,5 +2,27 @@ provider "aws" {
   region = "us-east-2"
 }
 
+module "function" {
+  source = "https://github.com/brikis98/devops-book/tree/main/ch3/tofu/modules/lambda"
 
+  name = var.name
 
+  src_dir = "${path.module}/src"
+  runtime = "nodejs20.x"
+  handler = "index.handler"
+
+  memory_size = 128
+  timeout     = 5
+
+  environment_variables = {
+    NODE_ENV = "production"
+  }
+}
+
+module "gateway" {
+  source = "github.com/vlad0sn/2e_git_td5//td5/scripts/tofu/modules/api-gateway"
+
+  name = var.name
+  function_arn       = module.function.function_arn
+  api_gateway_routes = ["GET /"]
+}
